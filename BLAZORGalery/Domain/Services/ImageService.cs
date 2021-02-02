969dede33f8415644eab8841f1809ea7be0938c3
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-02-02 20:19:48 -0300

    Create ImageService.cs

---
+++ BLAZORGalery/Domain/Services/ImageService.cs
@@ -0,0 +1,26 @@
+﻿using BLAZORGalery.Domain.Interfaces;
+using BLAZORGalery.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BLAZORGalery.Domain.Services
+{
+    public class ImageService : IImageService
+    {
+        public async Task<IEnumerable<ImageModel>> GetAllAsync(DateTime? createDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AddAsync(ImageModel image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

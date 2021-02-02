bfe402524a2b47280f9df534d351c23af9b3b564
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-02-02 20:19:35 -0300

    Create IImageService.cs

---
+++ BLAZORGalery/Domain/Interfaces/IImageService.cs
@@ -0,0 +1,14 @@
+﻿using BLAZORGalery.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BLAZORGalery.Domain.Interfaces
+{
+    public interface IImageService
+    {
+        Task<IEnumerable<ImageModel>> GetAllAsync(DateTime? createDate);
+        Task AddAsync(ImageModel image);
+        Task DeleteAsync(string id);
+    }
+}

392039ef007a248d109e2dc10ef84ad74c328879
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-02-02 10:18:55 -0300

    Create ImageModel.cs

---
+++ BLAZORGalery/Domain/Models/ImageModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BLAZORGalery.Domain.Models
+{
+    public class ImageModel
+    {
+        public ImageModel(string id, string imageSRC, DateTime createDate)
+        {
+            Id = id;
+            ImageSRC = imageSRC;
+            CreateDate = createDate;
+        }
+
+        public ImageModel(string imageSRC)
+        {
+            Id = Guid.NewGuid().ToString().Replace("-", string.Empty).Substring(0, 10);
+            ImageSRC = imageSRC;
+            CreateDate = DateTime.Now;
+        }
+
+        public string Id { get; set; }
+        public string ImageSRC { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}

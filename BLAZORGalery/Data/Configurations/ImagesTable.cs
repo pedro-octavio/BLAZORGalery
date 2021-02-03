4c580da07be20b8a7631e602053d6acb8c7c9174
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-02-03 13:46:50 -0300

    Create ImagesTable.cs

---
+++ BLAZORGalery/Data/Configurations/ImagesTable.cs
@@ -0,0 +1,24 @@
+﻿using BLAZORGalery.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BLAZORGalery.Data.Configurations
+{
+    internal static class ImagesTable
+    {
+        internal static void Configure(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ImageModel>()
+                .Property(x => x.Id)
+                .HasMaxLength(10)
+                .IsRequired();
+
+            modelBuilder.Entity<ImageModel>()
+                .Property(x => x.ImageSRC)
+                .IsRequired();
+
+            modelBuilder.Entity<ImageModel>()
+                .Property(x => x.CreateDate)
+                .IsRequired();
+        }
+    }
+}

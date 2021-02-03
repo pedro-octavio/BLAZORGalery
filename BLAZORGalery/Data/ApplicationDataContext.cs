b240d80704f969ad46cb92c10aa25f1ad455bc66
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-02-03 13:41:57 -0300

    Create ApplicationDataContext.cs

---
+++ BLAZORGalery/Data/ApplicationDataContext.cs
@@ -0,0 +1,18 @@
+﻿using BLAZORGalery.Data.Configurations;
+using BLAZORGalery.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BLAZORGalery.Data
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> dbContextOptions) : base(dbContextOptions) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ImagesTable.Configure(modelBuilder);
+        }
+
+        public DbSet<ImageModel> Images { get; set; }
+    }
+}

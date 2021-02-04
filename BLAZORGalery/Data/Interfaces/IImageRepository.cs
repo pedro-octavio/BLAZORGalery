415c2fc29825577f8dee81350a7de642cf01c225
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-02-04 11:33:43 -0300

    Image interface and repository created

---
+++ BLAZORGalery/Data/Interfaces/IImageRepository.cs
@@ -0,0 +1,15 @@
+﻿using BLAZORGalery.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BLAZORGalery.Data.Interfaces
+{
+    public interface IImageRepository
+    {
+        Task<IEnumerable<ImageModel>> GetAllAsync(DateTime? createDate);
+        Task<ImageModel> GetByIdAsync(string id);
+        Task AddAsync(ImageModel image);
+        Task DeleteAsync(ImageModel image);
+    }
+}
---
+++ BLAZORGalery/Data/Repositories/ImageRepository.cs
@@ -0,0 +1,55 @@
+﻿using BLAZORGalery.Data.Interfaces;
+using BLAZORGalery.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BLAZORGalery.Data.Repositories
+{
+    public class ImageRepository : IImageRepository
+    {
+        public ImageRepository(ApplicationDataContext applicationDataContext)
+        {
+            this.applicationDataContext = applicationDataContext;
+        }
+
+        private readonly ApplicationDataContext applicationDataContext;
+
+        public async Task<IEnumerable<ImageModel>> GetAllAsync(DateTime? createDate)
+        {
+            switch (createDate != null)
+            {
+                case true: return await applicationDataContext.Images.Where(x => x.CreateDate <= createDate).ToListAsync();
+                case false: return await applicationDataContext.Images.ToListAsync();
+            }
+        }
+
+        public async Task<ImageModel> GetByIdAsync(string id)
+        {
+            var image = await applicationDataContext.Images.FindAsync(id);
+
+            if (image != null)
+            {
+                applicationDataContext.Entry(image).State = EntityState.Detached;
+            }
+
+            return image;
+        }
+
+        public async Task AddAsync(ImageModel image)
+        {
+            await applicationDataContext.Images.AddAsync(image);
+
+            await applicationDataContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(ImageModel image)
+        {
+            applicationDataContext.Images.Remove(image);
+
+            await applicationDataContext.SaveChangesAsync();
+        }
+    }
+}

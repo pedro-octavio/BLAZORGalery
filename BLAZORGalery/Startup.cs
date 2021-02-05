7d677a3cbb1a16b3730f52cc77c33e0d00c5da32
pedro-octavio <pedrooctavio.dev@outlook.com>
2021-02-05 09:48:32 -0300

    Update Startup.cs

--- BLAZORGalery/Startup.cs
+++ BLAZORGalery/Startup.cs
@@ -1,5 +1,11 @@
+using BLAZORGalery.Data;
+using BLAZORGalery.Data.Interfaces;
+using BLAZORGalery.Data.Repositories;
+using BLAZORGalery.Domain.Interfaces;
+using BLAZORGalery.Domain.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,6 +23,11 @@ namespace BLAZORGalery
             services.AddRazorPages();
 
             services.AddServerSideBlazor();
+
+            services.AddDbContext<ApplicationDataContext>(x => x.UseSqlite(Configuration.GetConnectionString("BLAZORGaleryDB")));
+
+            services.AddScoped<IImageService, ImageService>();
+            services.AddScoped<IImageRepository, ImageRepository>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
